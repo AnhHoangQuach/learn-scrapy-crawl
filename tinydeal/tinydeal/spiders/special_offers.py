e9b04d40ddb6615e3c883b8db15ee6baa68b2620
AnhHoangQuach <hoanganh36.work@gmail.com>
2021-11-29 11:13:04 +0700

    feat: add project worldometers and tinydeal

---
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class SpecialOffersSpider(scrapy.Spider):
+    name = 'special_offers'
+    allowed_domains = ['www.tinydeal.com.hk/specials.html']
+    start_urls = ['http://www.tinydeal.com.hk/specials.html/']
+
+    def parse(self, response):
+        pass
